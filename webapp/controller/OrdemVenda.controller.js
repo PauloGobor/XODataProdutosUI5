a03113681cf09f555aac34adc349166225570202
PauloGobor <pauloegobor@gmail.com>
2020-04-12 14:03:12 +0000

    OrdemVenda JS,XML

--- webapp/controller/OrdemVenda.controller.js
+++ webapp/controller/OrdemVenda.controller.js
@@ -3,41 +3,27 @@ sap.ui.define([
 ], function (Controller) {
 	"use strict";
 
-	return Controller.extend("br.com.infoprodutos.Produtos.controller.Detalhes", {
+	return Controller.extend("br.com.infoprodutos.Produtos.controller.OrdemVenda", {
 
 		/**
 		 * Called when a controller is instantiated and its View controls (if available) are already created.
 		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf br.com.infoprodutos.Produtos.view.Detalhes
+		 * @memberOf br.com.infoprodutos.Produtos.view.OrdemVenda
 		 */
 		onInit: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.getRoute("Detalhes").attachMatched(this._onRoute, this);
-
+			oRouter.getRoute("OrdemVenda").attachMatched(this._onRoute, this);
 		},
-		
-		//acesso lista de ordens begin
-			funcListItemOrderPress: function (evt) {
-					var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-					//acessa o router
-					var selectedIdOrder = evt.getSource().getBindingContext().getProperty("OrderID");
-					//acesso o contexto de selecao
-					oRouter.navTo("OrdemVenda", {
-						///acessa o router detalhes
-						idOrder: selectedIdOrder
-					});
-				},
-		// acesso lista ordem end
 
 		_onRoute: function (evt) {
 			var oArgs = evt.getParameter("arguments"); //jogamntdo os argumenos do route( todos os paramentros )
 			var oView = this.getView();
 
-			var sURL = "/Products(" + oArgs.idProd + ")";
+			var sURL = "/Orders(" + oArgs.idOrder + ")";
 			oView.bindElement({
 				path: sURL, //qual a entidade que vou fazer a chamada.(caminho)
 				parameters: {
-					expand: "Category" // pega as informacoes da categoria que pertence aquele produto
+					expand: "Order_Details" // pega as informacoes da categoria que pertence aquele produto
 				},
 				events: {
 					dataRequested: function () {
@@ -50,21 +36,15 @@ sap.ui.define([
 			});
 		},
 
-		onBeforeRendering: function () {
-			this.getView().byId("SFormFornecedor").bindElement("Supplier");
-		},
-		
-		onNavBack: function(){
+		onNavBack: function () {
 			var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-			oRouter.navTo("Produtos",{},false);
+			oRouter.navTo("Detalhes", {}, false);
 		}
-		
-		
 
 		/**
 		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
 		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf br.com.infoprodutos.Produtos.view.Detalhes
+		 * @memberOf br.com.infoprodutos.Produtos.view.OrdemVenda
 		 */
 		//	onBeforeRendering: function() {
 		//
@@ -73,7 +53,7 @@ sap.ui.define([
 		/**
 		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
 		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf br.com.infoprodutos.Produtos.view.Detalhes
+		 * @memberOf br.com.infoprodutos.Produtos.view.OrdemVenda
 		 */
 		//	onAfterRendering: function() {
 		//
@@ -81,7 +61,7 @@ sap.ui.define([
 
 		/**
 		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf br.com.infoprodutos.Produtos.view.Detalhes
+		 * @memberOf br.com.infoprodutos.Produtos.view.OrdemVenda
 		 */
 		//	onExit: function() {
 		//
